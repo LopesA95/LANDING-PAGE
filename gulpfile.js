ea462cb68f398aaad09f8fa9f050f8462f6cbb03
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-09 02:20:50 -0400

    fix: correção de bugs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const imagemin = require("gulp-imagemin");
 const browserSync = require("browser-sync").create();
 const uglify = require("gulp-uglify");
 
+// Function to minify JavaScript files
 function scripts() {
   return gulp
     .src("./src/scripts/**/*.js")
@@ -11,6 +12,7 @@ function scripts() {
     .pipe(gulp.dest("./dist/scripts"));
 }
 
+// Function to compile Sass files and minify CSS
 function compileStyles() {
   return gulp
     .src("./src/styles/*.scss")
@@ -19,6 +21,7 @@ function compileStyles() {
     .pipe(browserSync.stream()); // Stream changes to browserSync for CSS injection
 }
 
+// Function to optimize images
 function optimizeImages() {
   return gulp
     .src("./src/assets/**/*")
@@ -26,18 +29,30 @@ function optimizeImages() {
     .pipe(gulp.dest("./dist/assets"));
 }
 
+// Function to watch files for changes and trigger appropriate tasks
 function watchFiles() {
+  // Initialize browserSync server
   browserSync.init({
     server: {
       baseDir: "./",
     },
   });
 
+  // Watch Sass files for changes and trigger compilation
   gulp.watch("./src/styles/**/*.scss", compileStyles);
+
+  // Watch image files for changes and trigger optimization
   gulp.watch("./src/assets/**/*", optimizeImages);
-  gulp.watch("./*.html").on("change", browserSync.reload); // Reload on HTML file changes
+
+  // Reload the browser when HTML files change
+  gulp.watch("./*.html").on("change", browserSync.reload);
+
+  // Watch JavaScript files for changes and trigger minification
   gulp.watch("./src/scripts/*.js", scripts);
 }
 
+// Default task to run all tasks in parallel
 exports.default = gulp.parallel(compileStyles, optimizeImages, scripts);
+
+// Task to watch files for changes
 exports.watch = watchFiles;
